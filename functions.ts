fa5ab08016b2237d6873fc200844796b1bd83841
William <wilenrike@gmail.com>
2021-02-13 22:33:08 -0600

    Se agregó e implementó la funcion QuickSort2

--- functions.ts
+++ functions.ts
@@ -7,6 +7,7 @@ function getPivot2(value : number) : number{
     return value % 2 == 0 ? value : value + 0.5;
 }
 
+//Funcion QuickSort
 function quickSort(low: number, high: number, array : number[]) : void{
     let i = low;
     let j = high;
@@ -35,6 +36,32 @@ function quickSort(low: number, high: number, array : number[]) : void{
     }
 }
 
+//Función QuickSort2
+function partition(array: number[], low: number, high: number) : number{
+    let pivot = array[high];
+
+    let i: number = low-1;
+
+    for(let j=low; j <= high-1; j++){
+        if(array[j] < pivot){
+            i++;
+            swap(i, j, array);
+        }
+    }
+    swap(i+1, high, array);
+    return i+1;
+}
+
+function quickSort2(array: number[], low: number, high: number) : void{
+    if(low < high){
+        let pi: number = partition(array, low, high);
+
+        quickSort2(array, low, pi-1);
+        quickSort2(array, pi+1, high);
+    }
+}
+
+//Función BubleSort
 function swap(i : number, j: number, array : number[]) : void{
     const temp = array[i];
     array[i] = array[j];
@@ -59,3 +86,7 @@ bubbleSort(collection);
 
 //Ordenando el array con quick sort
 quickSort(1, 88, collection);
+
+
+//Ordenando el array con quick sort 2
+quickSort2(collection, 1, 88);
